b6acd05ba07e31c09a670eb2fe1169380a8f230a
eightynine77 <175763558+eightynine77@users.noreply.github.com>
2026-02-02 18:19:58 +0700

    uploaded all files

---
+++ YTsearch/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace YTsearch.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ YTsearch/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace YTsearch.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
